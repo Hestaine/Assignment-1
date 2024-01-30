a1df41645644cc1fce9320121662e61ae24e6ba1
Hestaine <ariffhestaine@gmail.com>
2024-01-30 12:58:52 +0100

    yoyhyohyoyh

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Values and Variables
 const country = "Nigeria";
-const continent = "Africa";
-let population = "206 million";
+const myCountryContinent = "Africa";
+let myCountryPopulation = 206000000;
 
 console.log(country);
-console.log(continent);
-console.log(population);
+console.log(myCountryContinent);
+console.log(myCountryPopulation);
 
 // Data Types
 let isIsland = true;
@@ -14,8 +14,28 @@ let language;
 console.log(isIsland);
 console.log(language);
 
-// Let conat Var
+// Let, const, Var
 language = "English";
 console.log(language);
 
-// country = "Europe";
+// Basic Operators
+let myCountryInHalf = myCountryPopulation / 2;
+console.log(myCountryInHalf);
+
+let countryPlusOne = myCountryPopulation + 1;
+console.log(countryPlusOne);
+
+let finlandPopulation = 6000000;
+
+let isMyCountryMore = myCountryPopulation > finlandPopulation;
+console.log(isMyCountryMore);
+
+const myCountryDescription =
+  country +
+  " is in " +
+  myCountryContinent +
+  ", and some of its 206 million people speak " +
+  language;
+console.log(myCountryDescription);
+
+// Strings and Template Literals
