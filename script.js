6a79042f49e360afd497f821ba7a2f6d7a688aba
Hestaine <ariffhestaine@gmail.com>
2024-01-30 11:37:30 +0100

    yhoyhoyhyoyhoyhl

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let country = "Nigeria";
 let continent = "Africa";
 let population = "206 million";
+let isIsland = true;
+let language;
 
 console.log(country);
 console.log(continent);
 console.log(population);
-
-let isIsland = true;
-let language;
+console.log(isIsland);
+console.log(language);
